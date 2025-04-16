ecc5605b06236a8cf4c8a1566aeef527b7457f25
TheHouseofHER <her@thehouseofher.ai>
2025-04-16 14:04:57 -0400

    add debug route to test blank page

---
+++ app/app/debugtest/page.tsx
@@ -0,0 +1,7 @@
+export default function DebugTestPage() {
+  return (
+    <div style={{ padding: '50px', fontSize: '20px', color: 'black' }}>
+      <p>Hello from HER Debug Route!</p>
+    </div>
+  )
+}
