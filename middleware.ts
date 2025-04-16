fec5355a83912047107a535e1f1afb7dc562df3b
TheHouseofHER <her@thehouseofher.ai>
2025-04-16 05:01:11 -0400

    Temp: disable middleware for public access

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,3 @@
-import NextAuth from 'next-auth';
-
-import { authConfig } from '@/app/(auth)/auth.config';
-
-export default NextAuth(authConfig).auth;
-
-export const config = {
-  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
-};
+export function middleware() {
+  return new Response(null)
+}
